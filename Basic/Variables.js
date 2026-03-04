bf5245a773f5ebd6013bc00259e271279b725ae4
Manish Jaryal <manishjaryal51@gmail.com>
2026-03-04 11:29:52 +0530

    varible updated

--- Basic/Variables.js
+++ Basic/Variables.js
@@ -8,3 +8,22 @@ console.log(user_name);
 
 const dob = "01/08/2001";
 console.log(dob);
+
+/*
+key facts about variable in js
+var-> multiple declaration is possible
+var->global scoped can be accesed outside the func or block:
+const-> can not be updated
+*/
+
+var user_id = 121;
+var user_id = 122; //allowed
+
+if (true) {
+  var age = 24;
+  let score = 500;
+}
+console.log(age); // fine can be accesed outside the scope
+// console.log(score);//error
+const power = "high";
+// power = "low";//error
