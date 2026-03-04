0e5663da8d2ea4112b99a5edc4fc921912b00f30
Manish Jaryal <manishjaryal51@gmail.com>
2026-03-04 11:00:21 +0530

    variables

---
+++ Basic/Variables.js
@@ -0,0 +1,10 @@
+var message = "hello geeks";
+console.log(message);
+message = "hello developers!!";
+console.log(message);
+
+let user_name = "manish";
+console.log(user_name);
+
+const dob = "01/08/2001";
+console.log(dob);
