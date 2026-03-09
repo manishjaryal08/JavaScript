d9151ed5bc8115035ddfeb4ac350e0d54d1f9e46
Manish Jaryal <manishjaryal51@gmail.com>
2026-03-09 11:13:56 +0530

    data types

---
+++ Basic/DataTypes.js
@@ -0,0 +1,51 @@
+/*
+let userName = "Manish Jaryal";
+console.log(`${typeof userName} .... ${userName}`);
+
+let userId = 123;
+console.log(`${typeof userId} .... ${userId}`);
+
+let logedIn = true;
+console.log(`${typeof logedIn} .... ${logedIn}`);
+
+let userMail;
+console.log(`${typeof userMail} .... ${userMail}`);
+
+let panId = null;
+console.log(`${typeof panId} .... ${panId}`);
+
+let user1 = {
+  name: "Manish jaryal",
+  age: 24,
+  userid: "mansih@123",
+};
+console.log(user1.name);
+
+let id = "a" / 2;
+console.log(id);
+console.log(typeof id);
+
+
+let key1 = Symbol("password 12345");
+let key2 = Symbol("password 12345");
+console.log(key2 == key1);
+
+let num = 5664;
+console.log(typeof num);
+
+
+function greeting(name) {
+  console.log(typeof name);
+
+  return `hello ${name}!!`;
+}
+console.log(greeting("Manish"));
+
+
+let date = new Date();
+console.log(date.toLocaleDateString());
+*/
+
+let pattern = /hello/;
+let greeting = "hello developers !!";
+console.log(pattern.test(greeting));
