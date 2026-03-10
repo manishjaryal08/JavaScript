c6cb5ec6ad13f5735332403c97d1abf733c37b15
Manish Jaryal <manishjaryal51@gmail.com>
2026-03-10 12:02:03 +0530

    arithmetic operator

---
+++ Basic/Operators.js
@@ -0,0 +1,13 @@
+let num1 = 15;
+let num2 = 5;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** 2);
+console.log(num1++);
+console.log(num2++);
+console.log(++num1);
+console.log(++num2);
