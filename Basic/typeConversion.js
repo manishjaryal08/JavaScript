2c2a5febc51eefc7c284b482553d85e91c220ead
Manish Jaryal <manishjaryal51@gmail.com>
2026-03-19 18:29:32 +0530

    re-start

--- Basic/typeConversion.js
+++
@@ -1,6 +0,0 @@
-let a = "12";
-let b = "56";
-
-console.log(Number(a) + Number(b));
-console.log(a + b + "......." + Number(a) + Number(b));
-console.log(Number(a) + Number(b) + "......." + a + b);
--- Basic/userInput.js
+++
@@ -1,8 +0,0 @@
-const readline = require("readline-sync");
-let input = readline.question("what is your name");
-console.log(typeof input);
-let num;
-if (Number(input) != NaN) {
-  num = Number(input);
-}
-console.log(num + 5);
