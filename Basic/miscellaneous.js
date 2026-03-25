9ff3571d2c3a021f8b065463a99cbcd8b6fc0860
Manish Jaryal <manishjaryal51@gmail.com>
2026-03-25 16:34:58 +0530

    miscellaneous topics

---
+++ Basic/miscellaneous.js
@@ -0,0 +1,18 @@
+/*
+
+//Number
+let a = 10;
+let b = 3866.5767;
+let c = b.toFixed(2); //tofixed() function will return the string
+// console.log(c, typeof c);
+
+let d = b.toPrecision(6); //toPrecision() return string
+// console.log(d);
+
+let num1 = new Number(5);
+let num2 = new Number(5);
+
+console.log(num1 == num2); //compare the memory refrence
+console.log(num1.valueOf() == num2.valueOf()); //compare value
+
+ */
