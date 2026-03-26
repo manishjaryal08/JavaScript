5b341907f96b8a76e8a6e3ff338d1557843fad65
Manish Jaryal <manishjaryal51@gmail.com>
2026-03-26 15:25:31 +0530

    date file modified

--- level-2/Date.js
+++ level-2/Date.js
@@ -15,5 +15,5 @@ console.log(now.getMonth()); //month start form 0 ...0=january
 const date = new Date(0);
 console.log(date.toString());
 
-const time = Date.now();
+const time = Date.now(); //return time in mili sec from Thu Jan 01 1970 05:30:00 GMT+0530 (India Standard Time)
 console.log(time);
