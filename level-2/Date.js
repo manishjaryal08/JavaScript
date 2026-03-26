2486be117a48f1c25a708a6214f36bef789ad0dd
Manish Jaryal <manishjaryal51@gmail.com>
2026-03-26 15:14:40 +0530

    date object

---
+++ level-2/Date.js
@@ -0,0 +1,19 @@
+//Date
+// const now = new Date();
+/*
+
+console.log(now.toString());
+console.log(now.toISOString());
+console.log(now.toLocaleString());
+
+// console.log(tm.split(" "));
+
+console.log(now.getDay());
+console.log(now.getMonth()); //month start form 0 ...0=january
+*/
+
+const date = new Date(0);
+console.log(date.toString());
+
+const time = Date.now();
+console.log(time);
